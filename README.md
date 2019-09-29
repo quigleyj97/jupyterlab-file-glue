a695fbbd170c55700b5fe921b6991671edb320be
Joe Quigley <quigley.joseph@outlook.com>
2019-09-29 18:31:49 -0400

    Add: Plugin schema, update application

---
+++ README.md
@@ -0,0 +1,4 @@
+# `jupyterlab-file-glue`
+
+Plugin for customizing the mappings between file extensions and JupyterLab
+viewers.
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
 import { JupyterFrontEndPlugin } from "@jupyterlab/application";
+import { ISettingRegistry } from "@jupyterlab/coreutils";
+import { JSONExt, JSONObject, JSONArray } from "@phosphor/coreutils";
+import { DisposableSet } from "@phosphor/disposable";
 
 const plugin: JupyterFrontEndPlugin<void> = {
     id: "jupyterlab-file-glue",
-    activate: (app) => {
-        console.log("Initialized file glue!");
+    autoStart: true,
+    requires: [
+        ISettingRegistry,
+    ],
+    activate: async (app, settings: ISettingRegistry) => {
+        const { docRegistry } = app;
+
+        let mappings = new DisposableSet();
+
+        async function pairMappings() {
+            if (mappings != null) {
+                mappings.dispose();
+            }
+            mappings = new DisposableSet();
+            const { composite } = await settings.get("jupyterlab-file-glue:plugin", "mappings");
+            if (JSONExt.isArray(composite)) {
+                (composite as IMapping[]).map((mapping: IMapping) => {
+                    const { extensions, mimeTypes, name, format } = mapping;
+    
+                    mappings.add(docRegistry.addFileType({
+                        extensions: [...extensions],
+                        mimeTypes: [...mimeTypes],
+                        name,
+                        fileFormat: format
+                    }));
+                })
+            }
+            console.log("Applied file glue!", composite);
+        }
+
+        await pairMappings();
+
+        settings.pluginChanged.connect((_s, p) => {
+            if (p === "jupyterlab-file-glue:plugin") {
+                return pairMappings();
+            }
+        });
     }
+}
+export default plugin;
+
+interface IMapping extends JSONObject {
+    extensions: string[];
+    mimeTypes: string[];
+    name: string;
+    format: "text" | "base64" | "json";
 }
